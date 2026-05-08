de35a5a28e03da2bb11a357a70f66a538ee7cf5b
Tanjil Hossain <tanjilhossain845@gmail.com>
2026-05-08 23:51:09 +0600

    fixed result bug

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ function calculateResult() {
       reviewHTML += `<span >${word}</span> `;
     } else {
       mistakes++;
-      reviewHTML += `<span class="wrong-word" data-title="${sourceWords[i]}">${word}</span> `;
+      reviewHTML += `<span class="wrong-word" data-title="${sourceWords[i] || ""}">${word}</span> `;
     }
   });
 
