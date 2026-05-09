edafea0a636cec745d40ec60b9e8057f2792885c
Tanjil Hossain <tanjilhossain845@gmail.com>
2026-05-09 23:45:53 +0600

    change mistake word color

--- script.js
+++ script.js
@@ -126,7 +126,7 @@ function calculateResult() {
       reviewHTML += `<span >${word}</span> `;
     } else {
       mistakes++;
-      reviewHTML += `<span class="wrong-word" data-title="${sourceWords[i] || ""}">${word}</span> `;
+      reviewHTML += `<span class="wrong-word" data-title="${sourceWords[i] || ""}">${word}</span>`;
     }
   });
 
